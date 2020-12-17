9718ad8a4e1e3f6f24adb91fbff77c6f7f7ffe56
xitaro <54146191+xitaro@users.noreply.github.com>
2020-12-17 12:15:49 -0300

    Add files via upload

---
+++ Algotirmos de Programação 2/Aula 09 - Atividade/salvar.php
@@ -0,0 +1,8 @@
+<?php
+
+$nome = $_POST["txtName"];
+$altura = $_POST["txtWeight"];
+$peso = $_POST["txtPound"];
+echo "Name: ".$name."\n"; 
+echo "Height: ".$height."\n"; 
+echo "Weight: ".$weight."\n"; 
